72b0ea03a1201c33641f76697f900e667a474a7f
ma-ya's create <hello.myscreate@gmail.com>
2023-08-16 10:59:39 +0900

    fix: body class ready func

--- src/components/util/topPageBodyReady.ts
+++ src/components/util/topPageBodyReady.ts
@@ -1,6 +1,11 @@
 export function topPageBodyReady(isTopPage: boolean){
   if(isTopPage) {
-    document.body.classList.add('is-ready');
+    if(document.body.classList.contains('is-ready')) {
+      document.body.classList.remove('is-ready');
+      document.body.classList.add('is-ready');
+    } else {
+      document.body.classList.add('is-ready');
+    }
   } else {
     document.body.classList.remove('is-ready');
   }

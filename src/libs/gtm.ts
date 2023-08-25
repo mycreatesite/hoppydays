32e5684c798ee1c5f4b467e4f5c647ab71dd49fe
ma-ya's create <hello.myscreate@gmail.com>
2023-08-25 23:42:19 +0900

    fix: GTM

--- src/libs/gtm.ts
+++ src/libs/gtm.ts
@@ -1,7 +1,7 @@
-export const gtmId = process.env.NEXT_PUBLIC_GTM_ID || ''
+export const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
 
 declare global {
   interface Window {
-    dataLayer: Record<string, unknown>[]
+    dataLayer: Record<string, unknown>[];
   }
 }

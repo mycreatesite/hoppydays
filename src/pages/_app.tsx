e4741e7503df2dd9a425aabc979b7d49bdf76d88
ma-ya's create <hello.myscreate@gmail.com>
2023-08-26 10:42:11 +0900

    fix: add GTM module

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,15 +11,13 @@ import Layout from "@/components/layouts/Layout";
 import "@acab/reset.css";
 import "@/styles/globals.scss";
 import { FontGlobal } from "@/components/util/FontGlobal";
-import GoogleTagManager, { GtmId } from '@/components/util/GoogleTagManager'
-import { gtmId } from '@/libs/gtm'
+import TagManager from "react-gtm-module";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   const router = useRouter();
-  
-  useEffect(() => {
 
+  // animation
+  useEffect(() => {
     scrollAddClass();
     scrollAddClass(".js-scrollAddClassFooter", "70%");
     scrollParallax(".js-scrollParallax-bubble", "y", 150, -150);
@@ -30,14 +28,17 @@ export default function App({ Component, pageProps }: AppProps) {
       scrollParallax(".js-scrollParallax-mainLogo", "y", 0, 100, "20%", "0%");
       scrollParallax(".js-scrollParallax-slideContainer", "x", 100, -100);
     });
-    
   }, [router.pathname]);
+
+  // GTM
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID as string });
+  }, []);
 
   return (
     <>
       <FontGlobal />
       <Head>
-        <GoogleTagManager gtmId={gtmId as GtmId} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <DefaultSeo {...SEO} />
